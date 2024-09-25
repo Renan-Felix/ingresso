9f9f280d9a1d1a1131fa3ccab2755abdd6ccfa7f
Renan Félix <renan.felix25@hotmail.com>
2024-09-25 17:17:02 -0300

    Versão 1.1

---
+++ js/app.js
@@ -0,0 +1,61 @@
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let quantidade = parseInt(document.getElementById('qtd').value);
+
+
+
+    if(tipoIngresso.value == 'pista'){
+        comprarPista(quantidade);
+
+    }else if(tipoIngresso.value == 'inferior'){
+        comprarInferior(quantidade);
+
+    }else{
+        comprarSuperior(quantidade);
+    }
+
+    
+
+    
+}
+
+function comprarPista(quantidade){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if(quantidade <= qtdPista && quantidade > 0){
+        qtdPista = qtdPista - quantidade;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+
+    }else{
+        alert('Quantidade indisponível para pista');
+    }
+}
+
+function comprarInferior(quantidade){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if(quantidade <= qtdInferior && quantidade > 0){
+        qtdInferior = qtdInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent =qtdInferior;
+        alert('Compra relizada com sucesso');
+
+    }else{
+        alert('Quantidade indisponível para tipo cadeira inferior');
+    }
+}
+
+function comprarSuperior(quantidade){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if(quantidade <= qtdSuperior && quantidade > 0){
+        qtdSuperior = qtdSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+        
+        
+    }else{
+        alert('Quantidade indisponível para tipo cadeira superior');
+    }
+}
+
